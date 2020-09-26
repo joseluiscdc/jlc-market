6490186eaba97b483c07afa610b2f310619b0bd9
José Luis <joseluis.jlc@gmail.com>
2020-09-26 18:24:22 -0300

    Init repository

---
+++ README.md
@@ -0,0 +1,1 @@
+# jhlc-market
---
+++ src/main/java/com/jlc/market/JlcMarketApplication.java
@@ -0,0 +1,13 @@
+package com.jlc.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JlcMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JlcMarketApplication.class, args);
+	}
+
+}
