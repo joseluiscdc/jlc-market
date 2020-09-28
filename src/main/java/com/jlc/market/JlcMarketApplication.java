63b4ae21cf88adc2892af0afbc316b9e04121fa5
José Luis <joseluis.jlc@gmail.com>
2020-09-28 18:36:59 -0300

    changes

--- src/main/java/com/jlc/market/JlcMarketApplication.java
+++ src/main/java/com/jlc/market/JlcMarketApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JlcMarketApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(JlcMarketApplication.class, args);
 	}
-
 }
