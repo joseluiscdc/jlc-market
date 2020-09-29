ebb7dfe41625e4233a3f597451c830bf7760973b
José Luis <joseluis.jlc@gmail.com>
2020-09-28 23:00:25 -0300

    refactoring

--- src/main/java/com/jlc/market/persistence/entity/ComprasProductoPK.java
+++ src/main/java/com/jlc/market/persistence/entity/ComprasProductoPK.java
@@ -27,4 +27,35 @@ public class ComprasProductoPK implements Serializable {
     public void setIdProducto(Integer idProducto) {
         this.idProducto = idProducto;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((idCompra == null) ? 0 : idCompra.hashCode());
+        result = prime * result + ((idProducto == null) ? 0 : idProducto.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ComprasProductoPK other = (ComprasProductoPK) obj;
+        if (idCompra == null) {
+            if (other.idCompra != null)
+                return false;
+        } else if (!idCompra.equals(other.idCompra))
+            return false;
+        if (idProducto == null) {
+            if (other.idProducto != null)
+                return false;
+        } else if (!idProducto.equals(other.idProducto))
+            return false;
+        return true;
+    }
 }
