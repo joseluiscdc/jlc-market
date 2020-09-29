84ff8af69b4e78e7348fd927591bd6b9d141c494
José Luis <joseluis.jlc@gmail.com>
2020-09-29 16:06:18 -0300

    delete test greet

--- src/main/java/com/jlc/market/JlcMarketApplication.java
+++ src/main/java/com/jlc/market/JlcMarketApplication.java
@@ -1,7 +1,12 @@
 package com.jlc.market;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class JlcMarketApplication {
